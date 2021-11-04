62b3ca890b297d2807fb7030a940531a6e10b3f4
Fernando Paschoeto <fernandopaschoeto@gmail.com>
2021-11-03 23:57:26 -0300

    22 IONIC - Projeto finalizado.

--- 22 - IONIC/gasolinaAlcool/src/app/home/home.page.ts
+++ 22 - IONIC/gasolinaAlcool/src/app/home/home.page.ts
@@ -6,7 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public resultado: String = "Resultado:";
+  public precoAlcool;
+  public precoGasolina;
+
+
+  calcular(){
+    // Validar se os campos foram preenchidos. 
+    if( this.precoAlcool && this.precoGasolina ){
+      let pAlcool = parseFloat(this.precoAlcool);
+      let pGasolina = parseFloat(this.precoGasolina);
+
+      let res = pAlcool / pGasolina; 
+      if (res >= 0.7){
+        this.resultado = "Melhor utilizar Gasolina";
+      } else {
+        this.resultado = "Melhor utilizar Álcool. ";
+      }
+
+    } else {
+      this.resultado = "Preencha corretamente os campos. ";
+    }
+  }
 
   constructor() {}
+
+
 
 }

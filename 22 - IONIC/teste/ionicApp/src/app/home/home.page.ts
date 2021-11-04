e5c0e65a523f8a98399e30d3024cd0da4a89be62
Fernando Paschoeto <fernandopaschoeto@gmail.com>
2021-11-03 21:42:20 -0300

    22 IONIC - Data Binding no Angular.

--- 22 - IONIC/teste/ionicApp/src/app/home/home.page.ts
+++ 22 - IONIC/teste/ionicApp/src/app/home/home.page.ts
@@ -9,4 +9,17 @@ export class HomePage {
 
   constructor() {}
 
+  public titulo: String = "Meu primeiro App";
+
+  public imagemRandomica: String = "https://cdn.stocksnap.io/img-thumbs/960w/mushroom-fungus_IJ0B3RFSXX.jpg";
+  public imagemLocal: String = "../assets/icone-celular.png";
+
+  public atualiza(): void {
+    this.titulo = "Texto alterado. "
+  }
+  public normaliza(): void {
+    this.titulo = "Meu primeiro App"
+  }
+
+
 }
